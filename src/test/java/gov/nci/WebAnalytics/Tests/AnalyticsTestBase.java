03254eb667dc273e78b4077a2cf18791014320c8
daquinohd <daquinohd@mail.nih.gov>
2018-06-04 14:33:00 -0400

    Moved manage().window().maximize() to before each test method

--- src/test/java/gov/nci/WebAnalytics/Tests/AnalyticsTestBase.java
+++ src/test/java/gov/nci/WebAnalytics/Tests/AnalyticsTestBase.java
@@ -19,6 +19,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -57,7 +58,22 @@ public class AnalyticsTestBase extends BaseClass {
 	protected static List<String> harList;
 	protected static List<AnalyticsBase> loadBeacons;
 	protected static List<AnalyticsBase> clickBeacons;
-	    
+	
+	/**
+	* Configuration information for a TestNG class (http://testng.org/doc/documentation-main.html): 
+	* @BeforeSuite: The annotated method will be run before all tests in this suite have run. 
+	* @AfterSuite: The annotated method will be run after all tests in this suite have run. 
+	* @BeforeTest: The annotated method will be run before any test method belonging to the classes inside the <test> tag is run. 
+	* @AfterTest: The annotated method will be run after all the test methods belonging to the classes inside the <test> tag have run. 
+	* @BeforeGroups: The list of groups that this configuration method will run before. 
+	* 				 This method is guaranteed to run shortly before the first test method that belongs to any of these groups is invoked. 
+	* @AfterGroups: The list of groups that this configuration method will run after. 
+	* 				This method is guaranteed to run shortly after the last test method that belongs to any of these groups is invoked. 
+	* @BeforeClass: The annotated method will be run before the first test method in the current class is invoked. 
+	* @AfterClass: The annotated method will be run after all the test methods in the current class have been run. 
+	* @BeforeMethod: The annotated method will be run before each test method. 
+	* @AfterMethod: The annotated method will be run after each test method.
+	**/	
 	@BeforeTest(groups = { "Analytics" })
 	@Parameters	
 	public void beforeTest() {
@@ -169,6 +185,12 @@ public class AnalyticsTestBase extends BaseClass {
 		logger = report.startTest(this.getClass().getSimpleName());
 	}
 
+	@BeforeMethod(groups = { "Analytics" })
+	public void preMaximize() throws RuntimeException {
+		// Reset our browser to full screen before each method
+		driver.manage().window().maximize();
+	}
+	
 	@AfterMethod(groups = { "Analytics" })
 	public void tearDown(ITestResult result) {
 		if (result.getStatus() == ITestResult.FAILURE) {
