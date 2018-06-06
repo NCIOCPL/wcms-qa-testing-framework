3b57b72e2c4779aa91d952f154820f5bc68fd23b
daquinohd <daquinohd@mail.nih.gov>
2018-06-06 17:00:08 -0400

    Added tests

--- src/test/java/gov/nci/WebAnalytics/Tests/PageLoad_Test.java
+++ src/test/java/gov/nci/WebAnalytics/Tests/PageLoad_Test.java
@@ -19,6 +19,8 @@ public class PageLoad_Test extends AnalyticsTestBase {
 		pageLoad = new PageLoad(driver);
 	}	
 
+	// TODO: refactor has..() methods
+	// TODO: fix hasEvent() arg
 	/// Load and click events have been captured
 	@Test(groups = { "Analytics" }, priority = 1)
 	public void testHarAndBeacons() {
@@ -26,7 +28,7 @@ public class PageLoad_Test extends AnalyticsTestBase {
 		harList = getHarUrlList(proxy);
 		loadBeacons = pageLoad.getLoadBeacons(harList);
 		Assert.assertTrue(harList.size() > 0);
-		Assert.assertTrue(loadBeacons.size() > 0);		
+		Assert.assertTrue(loadBeacons.size() > 0);
 		logger.log(LogStatus.PASS, "Load events have been captured.");
 	}
 	
@@ -54,11 +56,39 @@ public class PageLoad_Test extends AnalyticsTestBase {
 		Assert.assertTrue(firstLoadBeacon.channel.equals("NCI Homepage") || firstLoadBeacon.channel.contains("Research"));
 		Assert.assertFalse(firstLoadBeacon.channel.contains("some other string"));
 		Assert.assertTrue(firstLoadBeacon.events[0].contains("1"));
+		logger.log(LogStatus.PASS, "Home-and-back nav values are correct.");		
 	}
 	
-	/// Home-and-back navigation returns expected values
+	/// Home navigation returns expected values
 	@Test(groups = { "Analytics" })
-	public void testHome() {
-		Assert.assertTrue(1 == 1);
+	public void testHomeLoad() {
+		pageLoad.gotoHomePage();
+		loadBeacons = pageLoad.getLoadBeacons(getHarUrlList(proxy));
+		Assert.assertTrue(hasEvent(loadBeacons, "event1"));
+		Assert.assertTrue(hasEvent(loadBeacons, "event47"));
+		Assert.assertTrue(hasProp(loadBeacons, 3, "/"));
+		Assert.assertTrue(hasProp(loadBeacons, 6, "Comprehensive Cancer Information"));
+		Assert.assertTrue(hasProp(loadBeacons, 44, "NCI Homepage"));
+		Assert.assertTrue(haseVar(loadBeacons, 1, "www.cancer.gov/"));
+		Assert.assertTrue(haseVar(loadBeacons, 44, "NCI Homepage"));		
+		logger.log(LogStatus.PASS, "Home page nav values are correct.");
 	}
+	
+	/// CTHP navigation returns expected values
+	@Test(groups = { "Analytics" })
+	public void testCTHPPatientLoad() {
+		pageLoad.gotoCTHPPatient();
+		loadBeacons = pageLoad.getLoadBeacons(getHarUrlList(proxy));
+		Assert.assertTrue(hasEvent(loadBeacons, "event1"));
+		Assert.assertTrue(hasEvent(loadBeacons, "event47"));
+		Assert.assertTrue(hasProp(loadBeacons, 8, "english"));
+		Assert.assertTrue(hasProp(loadBeacons, 6, "Bladder cancer"));
+		Assert.assertTrue(hasProp(loadBeacons, 10, "Bladder Cancer—Patient Version - National Cancer Institute"));
+		Assert.assertTrue(hasProp(loadBeacons, 44, "Cancer Types Landing Page"));
+		Assert.assertTrue(haseVar(loadBeacons, 1, "www.cancer.gov/types/bladder"));
+		Assert.assertTrue(haseVar(loadBeacons, 44, "Cancer Types Landing Page"));
+
+	}	
+	
+	
 }
