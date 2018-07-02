6a6d2952d58a2776167462f7cbc3c8e817854bc7
daquinohd <daquinohd@mail.nih.gov>
2018-07-02 11:29:13 -0400

    Reworking CTSBaiscSearch_Test

--- src/test/java/gov/nci/WebAnalytics/Tests/CTSBasicSearch_Test.java
+++ src/test/java/gov/nci/WebAnalytics/Tests/CTSBasicSearch_Test.java
@@ -1,19 +1,25 @@
 package gov.nci.WebAnalytics.Tests;
 
 import com.relevantcodes.extentreports.LogStatus;
+
+import gov.nci.WebAnalytics.AnalyticsRequest;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.nci.Utilities.Nav;
 import com.nci.clinicalTrial.pages.BasicSearch;
 
 public class CTSBasicSearch_Test extends AnalyticsTestBase{
 	
 	private BasicSearch basicSearch;
+	private AnalyticsRequest req;
 
 	@BeforeMethod(groups = { "Analytics" }) 
 	public void beforeMethod() {
 		basicSearch = new BasicSearch(driver);
+		req = new AnalyticsRequest();
 	}
 
 	/// ??? returns the expected general/shared values
@@ -23,11 +29,17 @@ public class CTSBasicSearch_Test extends AnalyticsTestBase{
 		logger.log(LogStatus.PASS, "CTS Basic gen value test passed.");
 	}		
 	
-	// @Test(groups = { "Analytics" })
+	@Test(groups = { "Analytics" })
 	public void testBasicBegin() {
-		// basicSearch.searchAge(age);
-		// basicSearch.beginBasicForm();
-		//  clickBeacons = basicSearch.getClickBeacons(getHarUrlList(proxy));
+
+		/* Do browser actions  **/ 
+		driver.navigate().to(Nav.basicSearchPage);
+		basicSearch.enterCancerType("stomatitis");		
+		
+		/* Get our beacon object **/ 
+		clickBeacons = req.getClickBeacons(getHarUrlList(proxy));
+		
+		/* Do assertions and log result */ 
 		Assert.assertTrue(hasEvent(clickBeacons, "event38"));
 		Assert.assertFalse(hasEvent(clickBeacons, "event40"));
 		Assert.assertTrue(hasProp(clickBeacons, 74, "clinicaltrials_basic|start"));
@@ -35,14 +47,19 @@ public class CTSBasicSearch_Test extends AnalyticsTestBase{
 		logger.log(LogStatus.PASS, "CTS Basic 'start' value test passed.");
 	}
 	
-	//@Test(groups = { "Analytics" })
+	@Test(groups = { "Analytics" })
 	public void testBasicAbandon() {
+		driver.navigate().to(Nav.basicSearchPage);
+		basicSearch.enterCancerType("liver");
+		driver.navigate().to(Nav.homePage);
+
 		// basicSearch.abandonBasicForm();
-		// clickBeacons = basicSearch.getClickBeacons(getHarUrlList(proxy));
+		clickBeacons = req.getClickBeacons(getHarUrlList(proxy));
+		
 		Assert.assertTrue(hasEvent(clickBeacons, 40));
-		//Assert.assertTrue(hasProp(clickBeacons, 74, "clinicaltrials_basic|abandon|a"));
-		//Assert.assertTrue(haseVar(clickBeacons, 47, "clinicaltrials_basic"));
+		Assert.assertTrue(hasProp(clickBeacons, 74, "clinicaltrials_basic|abandon|q"));
+		Assert.assertTrue(haseVar(clickBeacons, 47, "clinicaltrials_basic"));
 		logger.log(LogStatus.PASS, "CTS Basic 'abandon' value test passed.");
-	}	
+	}
 }
 
