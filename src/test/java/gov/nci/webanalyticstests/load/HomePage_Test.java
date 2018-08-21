8ce8e0f36f21759381dff16d78fbdaeb01898393
Dion d'Aquino <hendaqui@publicisgroupe.net>
2018-08-21 12:41:29 -0400

    Added homepage tests & assertions

--- src/test/java/gov/nci/webanalyticstests/load/HomePage_Test.java
+++ src/test/java/gov/nci/webanalyticstests/load/HomePage_Test.java
@@ -1,38 +1,26 @@
 package gov.nci.webanalyticstests.load;
 
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import gov.nci.WebAnalytics.AnalyticsPageLoad;
 import gov.nci.WebAnalytics.AnalyticsRequest;
 import gov.nci.webanalyticstests.AnalyticsTestBase;
 
 public class HomePage_Test extends AnalyticsTestBase {
 
-	private AnalyticsPageLoad analyticsPageLoad;
 	private AnalyticsRequest beacon;
-	
-	// TODO: Add Spanish, microsite home pages
-	
-	@BeforeMethod(groups = { "Analytics" }) 
-	public void setupPageLoad() throws MalformedURLException, UnsupportedEncodingException {
-		analyticsPageLoad = new AnalyticsPageLoad(driver);
-	}	
 
 	/// Home pageload returns expected values
 	@Test(groups = { "Analytics" })
 	public void testSiteHomeLoad() {
 		try {
 			System.out.println("Home page load event:");
-			analyticsPageLoad.gotoHomePage();
+			driver.get(config.goHome());
 			beacon = getLoadBeacon();
-			Assert.assertTrue(beacon.hasEvent(1));
-			Assert.assertTrue(beacon.hasEvent(47));
+			AssertCommon();
 			Assert.assertTrue(beacon.hasProp(3, "/"));
 			Assert.assertTrue(beacon.hasProp(6, "Comprehensive Cancer Information"));
 			Assert.assertTrue(beacon.hasProp(44, "NCI Homepage"));
@@ -51,16 +39,17 @@ public class HomePage_Test extends AnalyticsTestBase {
 	public void testHomeAndBack() throws MalformedURLException {
 		try {
 			System.out.println("Home page and back load event:");
-			analyticsPageLoad.goHomeAndBack();
+			driver.get(config.goHome());
+			driver.get(config.getPageURL("SpanishPage"));
+			driver.get(config.goHome());
 			beacon = getLoadBeacon();
-			AnalyticsRequest firstLoadBeacon = loadBeacons.get(0);
-			String[] evts = firstLoadBeacon.getEvents();
-			// Temporary / debugging tests
-			//Assert.assertTrue(firstLoadBeacon.channel.equals("NCI Homepage") || firstLoadBeacon.channel.contains("Research"));
-			//Assert.assertFalse(firstLoadBeacon.channel.contains("some other string"));
-			Assert.assertTrue(evts[0].contains("1"));
+			AssertCommon();
+			Assert.assertTrue(beacon.hasProp(3, "/"));
+			Assert.assertTrue(beacon.hasProp(6, "Comprehensive Cancer Information"));
+			Assert.assertTrue(beacon.hasProp(44, "NCI Homepage"));
+			Assert.assertTrue(beacon.haseVar(1, "www.cancer.gov/"));
+			Assert.assertTrue(beacon.haseVar(44, "NCI Homepage"));
 			logger.log(LogStatus.PASS, "Home-and-back nav values are correct.");
-			
 		}
 		catch (Exception e) {
 			Assert.fail("Error loading Home-page-and-back.");
@@ -72,8 +61,14 @@ public class HomePage_Test extends AnalyticsTestBase {
 	public void testSpanishHomeLoad() {
 		try {
 			System.out.println("Spanish home page load event:");
-			// analyticsPageLoad.gotoSpanishHome();
-			Assert.assertEquals(1, 1);
+			driver.get(config.getPageURL("SpanishPage"));
+			beacon = getLoadBeacon();
+			AssertCommon();
+			Assert.assertTrue(beacon.hasProp(3, "/"));
+			Assert.assertTrue(beacon.hasProp(6, "Comprehensive Cancer Information"));
+			Assert.assertTrue(beacon.hasProp(44, "NCI Homepage"));
+			Assert.assertTrue(beacon.haseVar(1, "www.cancer.gov/"));
+			Assert.assertTrue(beacon.haseVar(44, "NCI Homepage"));
 			logger.log(LogStatus.PASS, "Spanish home page nav values are correct.");
 		}
 		catch (Exception e) {
@@ -86,14 +81,26 @@ public class HomePage_Test extends AnalyticsTestBase {
 	public void testMicrositeHomeLoad() {
 		try {
 			System.out.println("Microsite home load event:");
-			// analyticsPageLoad.gotoMicrositeHome();
-			Assert.assertEquals(1, 1);
+			driver.get(config.getPageURL("MicroSite"));
+			beacon = getLoadBeacon();
+			AssertCommon();
+			Assert.assertTrue(beacon.hasProp(3, "/"));
+			Assert.assertTrue(beacon.hasProp(6, "Comprehensive Cancer Information"));
+			Assert.assertTrue(beacon.hasProp(44, "NCI Homepage"));
+			Assert.assertTrue(beacon.haseVar(1, "www.cancer.gov/"));
+			Assert.assertTrue(beacon.haseVar(44, "NCI Homepage"));
 			logger.log(LogStatus.PASS, "Microsite home nav values are correct.");
 		}
 		catch (Exception e) {
 			Assert.fail("Error loading microsite home page.");
 			e.printStackTrace();
 		}
+	}
+	
+	// Common assertions
+	private void AssertCommon() {
+		Assert.assertTrue(beacon.hasEvent(1));
+		Assert.assertTrue(beacon.hasEvent(47));
 	}
 	
 }
