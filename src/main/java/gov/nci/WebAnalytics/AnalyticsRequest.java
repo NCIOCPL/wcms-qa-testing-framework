64cc108f9bd0179f735641056e876fc11ac99241
daquinohd <daquinohd@mail.nih.gov>
2018-08-23 11:34:34 -0400

    Moved reusable methods back into AnalyticsRequest

--- src/main/java/gov/nci/WebAnalytics/AnalyticsRequest.java
+++ src/main/java/gov/nci/WebAnalytics/AnalyticsRequest.java
@@ -1,9 +1,122 @@
 package gov.nci.WebAnalytics;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
 public class AnalyticsRequest {
 	
+	// TODO: Remove buildParamsList/getList when possible	
+	public URI uri;
+	public String url;
+	public List<NameValuePair> paramsList;		
+	
 	public AnalyticsRequest(String url) {
-		// System.out.println("Debug AnalyticsRequest...");
+		this.url = url;
+		this.uri = null;
+		this.paramsList = new ArrayList<NameValuePair>();
 	}
+	
+	/**
+	 * Build a collection of parameters
+	 * @throws NullPointerException
+	 */
+	public void buildParamsList() throws NullPointerException {
+		this.uri = createUri(url);
+		this.paramsList = this.getList(uri);
+	}
+	
+	/**
+	 * Create URI object from a given URL string
+	 * @param url (String)
+	 * @return URI
+	 */
+	private static URI createUri(String url) {
+		try {
+			URI rtnUri = URI.create(url);
+			return rtnUri;
+		} catch (IllegalArgumentException ex) {
+			System.out.println("Invalid request URL \"" + url + 
+					"\\\" at AnalyticsRequest:createURI()");
+			return null;
+		}
+	}
+	
+
+	
+	/**
+	 * Split URI into list of encoded elements
+	 * @param uri
+	 * @return retParams
+	 */
+	@Deprecated
+	public static List<NameValuePair> getList(URI uri) {
+		List<NameValuePair> rtnParams = new ArrayList<NameValuePair>();
+		
+		try {
+			String queries = uri.getRawQuery(); // get encoded query string
+			for(String parm : queries.split("&")) {
+				String[] pair = parm.split("=");
+				String Name = URLDecoder.decode(pair[0], "UTF-8");
+				String value = "";
+				if(pair.length > 1) {
+					value = URLDecoder.decode(pair[1], "UTF-8"); 
+				}
+				rtnParams.add(new BasicNameValuePair(Name, value));				
+			}
+		} 
+		catch (UnsupportedEncodingException ex) {
+			System.out.println("Error decoding URI in WaParams:buildParamsList()");
+		}		
+		return rtnParams;
+	}	
+	
+	/******************** Utility functions ****************************************/
+	/**
+	 * Utility function to check for a user-specified variable and value
+	 * @param name
+	 * @param value
+	 * @return bool
+	 */
+	protected boolean hasVariable(String name, String value) {
+		// TODO: fill this out
+		return false;
+	}
+	
+	/**
+	 * Get a list of numbered parameters and their values (e.g. [prop1="www.cancer.gov", prop2="/home", prop3="NCI"])
+	 * @param paramList
+	 * @param parm
+	 * @param replacement
+	 * @return
+	 */
+	protected static List<NameValuePair> getNumberedParams(List<NameValuePair> paramList, String parm, String replacement) {
+		List<NameValuePair> rtnList = new ArrayList<>();
+		for (NameValuePair param : paramList) {
+			// Regex: parameter name followed by 1 or more digits, starting with 1-9 only
+			if(param.getName().matches("^" + parm + "[1-9]\\d*$")) {
+				String rtnName = param.getName().replace(parm, replacement);
+				String rtnValue = param.getValue();
+				rtnList.add(new BasicNameValuePair(rtnName, rtnValue));
+			}
+		}
+		return rtnList;
+	}
+	
+	/**
+	 * Overload for getNumberedParams
+	 * Can be used for cases where the parameter name and analytics variable name match 
+	 * @param paramList
+	 * @param parm
+	 * @return
+	 */
+	protected static List<NameValuePair> getNumberedParams(List<NameValuePair> paramList, String parm) {
+		return getNumberedParams(paramList, parm, parm);
+	}	
 	
 }
--- src/main/java/gov/nci/WebAnalytics/Beacon.java
+++ src/main/java/gov/nci/WebAnalytics/Beacon.java
@@ -15,12 +15,11 @@ public class Beacon extends AnalyticsRequest {
 	// TODO: reuse collection method from ParsedURL() instead of the local getList()
 
 	// Constants
-	public static final String STATIC_SERVER = "static.cancer.gov";
 	public static final String TRACKING_SERVER = "nci.122.2o7.net";
 		
 	// Parameter values from URL
 	static final String CHANNEL = "ch";
-	static final String EVENTS = "events";	
+	static final String EVENTS = "events";
 	static final String LINKTYPE = "pe";
 	static final String LINKNAME = "pev2";
 	static final String LINKURL = "pev1";
@@ -30,47 +29,13 @@ public class Beacon extends AnalyticsRequest {
 	// to each of these query parameter
 	static final String PROP_PARTIAL = "c";
 	static final String EVAR_PARTIAL = "v";
-	static final String HIER_PARTIAL = "h";		
+	static final String HIER_PARTIAL = "h";
 	
-	public URI uri;
-	public String url;
-	public List<NameValuePair> paramsList;
-		
-	/**
-	 * Constructor with 'url' arg
-	 * @param url
-	 */
+	// Constructor
 	public Beacon(String url) {
 		super(url);
-		this.url = url;
-		this.uri = null;
-		this.paramsList = new ArrayList<NameValuePair>();
 	}
 	
-	/**
-	 * Build the 
-	 * @throws NullPointerException
-	 */
-	public void buildParamsList() throws NullPointerException {
-		this.uri = createUri(url);
-		this.paramsList = this.getList(uri);
-	}
-	
-	/**
-	 * Create URI object from a given URL string
-	 * @param url
-	 * @return
-	 */
-	private static URI createUri(String url) {
-		try {
-			URI rtnUri = URI.create(url);
-			return rtnUri;
-		} catch (IllegalArgumentException ex) {
-			System.out.println("Invalid request URL \"" + url + 
-					"\\\" at AnalyticsRequest:createURI()");
-			return null;
-		}
-	}
 	
 	/**
 	 * Get the reporting suites (s_account) value
@@ -212,37 +177,6 @@ public class Beacon extends AnalyticsRequest {
 			return true;
 		}
 	}
-
-	/**
-	 * Get a list of numbered parameters and their values (e.g. [prop1="www.cancer.gov", prop2="/home", prop3="NCI"])
-	 * @param paramList
-	 * @param parm
-	 * @param replacement
-	 * @return
-	 */
-	private static List<NameValuePair> getNumberedParams(List<NameValuePair> paramList, String parm, String replacement) {
-		List<NameValuePair> rtnList = new ArrayList<>();
-		for (NameValuePair param : paramList) {
-			// Regex: parameter name followed by 1 or more digits, starting with 1-9 only
-			if(param.getName().matches("^" + parm + "[1-9]\\d*$")) {
-				String rtnName = param.getName().replace(parm, replacement);
-				String rtnValue = param.getValue();
-				rtnList.add(new BasicNameValuePair(rtnName, rtnValue));
-			}
-		}
-		return rtnList;
-	}
-	
-	/**
-	 * Overload for getNumberedParams
-	 * Can be used for cases where the parameter name and analytics variable name match 
-	 * @param paramList
-	 * @param parm
-	 * @return
-	 */
-	private static List<NameValuePair> getNumberedParams(List<NameValuePair> paramList, String parm) {
-		return getNumberedParams(paramList, parm, parm);
-	}	
 		
 	/**************** Methods to check for given values in a beacon ****************/
 	/*
@@ -350,44 +284,5 @@ public class Beacon extends AnalyticsRequest {
 		}
 		return false;
 	}
-	
-	/**
-	 * Utility function to check for a user-specified analytics variable and value
-	 * @param name
-	 * @param value
-	 * @return bool
-	 */
-	public boolean hasVariable(String name, String value) {
-		// TODO: fill this out
-		return false;
-	}		
-
-	
-	/**
-	 * Split URI into list of encoded elements
-	 * @param uri
-	 * @return retParams
-	 */
-	@Deprecated
-	public static List<NameValuePair> getList(URI uri) {
-		List<NameValuePair> rtnParams = new ArrayList<NameValuePair>();
 		
-		try {
-			String queries = uri.getRawQuery(); // get encoded query string
-			for(String parm : queries.split("&")) {
-				String[] pair = parm.split("=");
-				String Name = URLDecoder.decode(pair[0], "UTF-8");
-				String value = "";
-				if(pair.length > 1) {
-					value = URLDecoder.decode(pair[1], "UTF-8"); 
-				}
-				rtnParams.add(new BasicNameValuePair(Name, value));				
-			}
-		} 
-		catch (UnsupportedEncodingException ex) {
-			System.out.println("Error decoding URI in WaParams:buildParamsList()");
-		}		
-		return rtnParams;
-	}
-	
 }
