cf513897ce3800024b2b9cce0f41dec384570c56
daquinohd <daquinohd@mail.nih.gov>
2018-10-03 17:34:35 -0400

    Added ClickUtil and dummy-proof JS click event

---
+++ src/main/java/gov/nci/Utilities/ClickUtil.java
@@ -0,0 +1,23 @@
+package gov.nci.Utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ClickUtil {
+
+	private static JavascriptExecutor javaScript;
+	
+	/**
+	 * Click method for cases where Selenium can't find or otherwise doesn't want to
+	 * click on a given WebElement.
+	 * 
+	 * @param driver
+	 * @param element
+	 */
+	public static void forceClick(WebDriver driver, WebElement element) {
+		javaScript = (JavascriptExecutor) driver;
+		javaScript.executeScript("arguments[0].click();", element);
+	}
+
+}
