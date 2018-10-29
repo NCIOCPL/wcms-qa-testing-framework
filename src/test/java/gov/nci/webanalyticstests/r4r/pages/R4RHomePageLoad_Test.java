c905396a23bf9c640ae09bda78818f389008bbb1
daquinohd <daquinohd@mail.nih.gov>
2018-10-29 15:00:59 -0400

    Fixed R4R results / home load events

--- src/test/java/gov/nci/webanalyticstests/r4r/pages/R4RHomePageLoad_Test.java
+++ src/test/java/gov/nci/webanalyticstests/r4r/pages/R4RHomePageLoad_Test.java
@@ -1,36 +1,23 @@
 package gov.nci.webanalyticstests.r4r.pages;
 
-import java.util.Iterator;
-
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import gov.nci.webanalytics.Beacon;
 import gov.nci.webanalyticstests.AnalyticsTestLoadBase;
 
-public class R4RAllPagesLoad_Test extends AnalyticsTestLoadBase {
+public class R4RHomePageLoad_Test extends AnalyticsTestLoadBase {
 
-	private final String TESTDATA_SHEET_NAME = "R4RPageLoad";
-
-	private String testDataFilePath;
-
-	// ==================== Setup methods ==================== //
-
-	@BeforeClass(groups = { "Analytics" })
-	private void setupClass() {
-		testDataFilePath = config.getProperty("AnalyticsR4RData");
-	}
+	private final String PATH = "/research/resources";
 
 	// ==================== Test methods ==================== //
 
 	/// Test R4R Pages Load event
-	@Test(dataProvider = "R4RPageLoad", groups = { "Analytics" })
-	public void testR4RPageLoad(String path, String type) {
-		System.out.println("Path: " + path);
-		System.out.println("Page type: " + type);
-		driver.get(config.goHome() + path);
+	@Test(groups = { "Analytics" })
+	public void testR4RPageLoad() {
+		System.out.println("Path: " + PATH);
+		System.out.println("Page type: R4R Home");
+		driver.get(config.goHome() + PATH);
 
 		try {
 			Beacon beacon = getBeacon();
@@ -39,13 +26,6 @@ public class R4RAllPagesLoad_Test extends AnalyticsTestLoadBase {
 			handleTestErrors(new Object() {
 			}, e);
 		}
-	}
-
-	// ==================== Data providers ==================== //
-
-	@DataProvider(name = "R4RPageLoad")
-	private Iterator<Object[]> getR4RPageLoadData() {
-		return getPathContentTypeData(testDataFilePath, TESTDATA_SHEET_NAME);
 	}
 
 	// ==================== Common assertions ==================== //
@@ -57,12 +37,12 @@ public class R4RAllPagesLoad_Test extends AnalyticsTestLoadBase {
 	 */
 	private void doCommonClassAssertions(Beacon beacon) {
 		Assert.assertTrue(beacon.hasSuite("nciglobal", driver.getCurrentUrl()), "Common missing Global Suite");
-		Assert.assertEquals(beacon.channels, "Research", "Channel");
+		Assert.assertEquals(beacon.channels, "Research", "channel");
 		Assert.assertTrue(beacon.hasEvent(1), "Missing event1");
 		Assert.assertTrue(beacon.hasEvent(47), "Missing event47");
 		Assert.assertEquals(beacon.props.get(6), "Resources for Researchers", "prop6");
 		Assert.assertEquals(beacon.props.get(10), "Resources for Researchers - National Cancer Institute", "prop10");
-		Assert.assertEquals(beacon.props.get(44), "Research Landing Page", "prop44");
+		Assert.assertEquals(beacon.props.get(44), "RandD Resources", "prop44");
 		Assert.assertEquals(beacon.eVars.get(44), beacon.props.get(44), "eVar44");
 	}
 }
---
+++ src/test/java/gov/nci/webanalyticstests/r4r/pages/R4RResultsPageLoad_Test.java
@@ -0,0 +1,49 @@
+package gov.nci.webanalyticstests.r4r.pages;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import gov.nci.webanalytics.Beacon;
+import gov.nci.webanalyticstests.AnalyticsTestLoadBase;
+
+public class R4RResultsPageLoad_Test extends AnalyticsTestLoadBase {
+
+	private final String PATH = "/research/resources/search";
+
+	// ==================== Test methods ==================== //
+
+	/// Test R4R Pages Load event
+	@Test(groups = { "Analytics" })
+	public void testR4RPageLoad() {
+		System.out.println("Path: " + PATH);
+		System.out.println("Page type: R4R Results Page");
+		driver.get(config.goHome() + PATH);
+
+		try {
+			Beacon beacon = getBeacon();
+			doCommonClassAssertions(beacon);
+		} catch (Exception e) {
+			handleTestErrors(new Object() {
+			}, e);
+		}
+	}
+
+	// ==================== Common assertions ==================== //
+
+	/**
+	 * Shared assertions for all tests in this class.
+	 * 
+	 * @param beacon
+	 */
+	private void doCommonClassAssertions(Beacon beacon) {
+		Assert.assertTrue(beacon.hasSuite("nciglobal", driver.getCurrentUrl()), "Common missing Global Suite");
+		Assert.assertEquals(beacon.channels, "Research", "channel");
+		Assert.assertTrue(beacon.hasEvent(1), "Missing event1");
+		Assert.assertTrue(beacon.hasEvent(47), "Missing event47");
+		Assert.assertEquals(beacon.props.get(6), "Resources for Researchers", "prop6");
+		Assert.assertEquals(beacon.props.get(10),
+				"Resources for Researchers: Search Results - National Cancer Institute", "prop10");
+		Assert.assertEquals(beacon.props.get(44), "RandD Resources", "prop44");
+		Assert.assertEquals(beacon.eVars.get(44), beacon.props.get(44), "eVar44");
+	}
+}
