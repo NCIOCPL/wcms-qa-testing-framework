bc915565516584cb23a419777158e745493a14eb
Volker Englisch <venglisch@users.noreply.github.com>
2018-11-09 16:53:11 -0500

    Adding test for header element (h1) visibility. OCEPROJECT-4805

--- src/test/java/gov/nci/dictionary/NewTerms_Test.java
+++ src/test/java/gov/nci/dictionary/NewTerms_Test.java
@@ -8,45 +8,40 @@ import java.util.List;
 
 import com.relevantcodes.extentreports.LogStatus;
 
-import org.testng.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NewTerms_Test extends NewDictionaryCommon {
 
 	private String language = "EN";
+    private String dictionary = "glossary";
 
-/*  ***************************** Test Methods ****************************************** */
-	// Testing to confirm the correct page title is displayed
+/* ***************************** Test Methods ****************************************** */
+	// Testing to confirm the page header (H1 element) is visible
 	// --------------------------------------------------------------------------------
 	@Test(dataProvider = "Glossary", groups = { "dictionary" })
-	public void TitleVisible(String url) {
-		DictionarySearch dict;
-		String dictTitle = "NCI Dictionary of Cancer Terms";
-		String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
+    public void HeaderVisible(String url) {
+        DictionarySearch dict;
+        String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
 
-		logger.log(LogStatus.INFO, "Testing if dictionary title is visible: " + dictTitle);
+        logger.log(LogStatus.INFO, "Testing if dictionary header element is visible");
+        driver.get(url);
 
-		throw new RuntimeException("This test is disabled pending resolution of build errors.");
+        try {
+            dict = new DictionarySearch(driver);
+            boolean headerVisible = dict.headerVisible();
+            String headerNotVisibleTxt = "*** Error: Cancer Term Dictionary Header"
+                                       + " Not Displayed ***";
+            Assert.assertTrue(headerVisible, headerNotVisibleTxt);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
+            Assert.fail("*** Error loading page in " + curMethod + " ***");
+        }
+    }
 
-//		driver.get(url);
-//
-//		try {
-//			dict = new DictionarySearch(driver);
-//			boolean titleVisible = dict.TitleVisible();
-//			WebElement titleText = dict.getTitleText();
-//			String titleNotVisible = "*** Error: Term Dictionary Header Not Visible ***";
-//			String wrongTitleText = "*** Error: Term Dictionary Title text mismatch ***";
-//
-//			Assert.assertTrue(titleVisible, titleNotVisible);
-//			Assert.assertTrue(titleText.getText().contains(dictTitle), wrongTitleText);
-//		} catch (MalformedURLException | UnsupportedEncodingException e) {
-//			Assert.fail("*** Error loading page in " + curMethod + " ***");
-//		}
-	}
 
 	// Confirm the link to the glossary widget in the header paragraph is
 	// visible.
