58240c08bbd5cd10a05a091fecf7d9c9608c4a9a
Volker Englisch <venglisch@users.noreply.github.com>
2018-11-29 15:52:40 -0500

    Drug Test dictionary home page URL. OCEPROJECT-4933

--- src/test/java/gov/nci/dictionary/NewDrugs_Test.java
+++ src/test/java/gov/nci/dictionary/NewDrugs_Test.java
@@ -87,6 +87,26 @@ public class NewDrugs_Test extends NewDictionaryCommon {
     }
 
 
+    // Confirming the dictionary home page displays the correct URL
+    // -----------------------------------------------------------------
+    @Test(dataProvider = "DrugDictionary", groups = { "dictionary" })
+    public void DictHomePageUrl(String url) {
+        DictionarySearch dict;
+        String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
+
+        logger.log(LogStatus.INFO, "Testing dictionary home page Url");
+        driver.get(url);
+
+        try {
+            dict = new DictionarySearch(driver);
+            boolean dictHomeUrl = dict.isHomePageUrl(dictionary);
+            String NotHomePageTxt = "*** Error: Drug Dictionary Home Page URL incorrect ***";
+            Assert.assertTrue(dictHomeUrl, NotHomePageTxt);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
+            Assert.fail("*** Error loading page in " + curMethod + " ***");
+        }
+    }
+
 /*  ***************************** Data Provider ***************************************** */
 
     // DataProvider to read the Excel spreadsheet with data containing URLs to be checked
