ebc0382a3fb3d8bede4a2d2624065fefff325b2f
Volker Englisch <venglisch@users.noreply.github.com>
2018-12-03 14:18:42 -0500

    Genetics - Test if search input field is visible. OCEPROJECT-4821 (#167)

--- src/test/java/gov/nci/dictionary/NewGenetics_Test.java
+++ src/test/java/gov/nci/dictionary/NewGenetics_Test.java
@@ -106,6 +106,28 @@ public class NewGenetics_Test extends NewDictionaryCommon {
             Assert.fail("*** Error loading page in " + curMethod + " ***");
         }
     }
+
+    // Confirming the search input field is displayed
+    // -------------------------------------------------------------------------
+    @Test(dataProvider = "GeneticsDictionary", groups = { "dictionary" })
+    public void SearchInputVisible(String url) {
+        DictionarySearch dict;
+        String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
+
+        logger.log(LogStatus.INFO, "Testing if search entry field is visible");
+        driver.get(url);
+
+        try {
+            dict = new DictionarySearch(driver);
+            boolean searchVisible = dict.isSearchInputVisible();
+            String searchErrorTxt = "*** Error: Glossary Dictionary Search Input "
+                                  + "Field Not Displayed ***";
+            Assert.assertTrue(searchVisible, searchErrorTxt);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
+            Assert.fail("*** Error loading page in " + curMethod + " ***");
+        }
+    }
+
 /*  ***************************** Data Provider ***************************************** */
 
     // DataProvider to read the Excel spreadsheet with data containing URLs to be checked
