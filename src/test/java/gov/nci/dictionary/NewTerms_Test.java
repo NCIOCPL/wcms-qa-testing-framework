663d34c2c976db2c2dabd98c712b745366759520
Volker Englisch <venglisch@users.noreply.github.com>
2019-01-03 16:18:40 -0500

    Glossary - Replace printing with logging. OCEPROJECT-4829

--- src/test/java/gov/nci/dictionary/NewTerms_Test.java
+++ src/test/java/gov/nci/dictionary/NewTerms_Test.java
@@ -282,7 +282,7 @@ public class NewTerms_Test extends NewDictionaryCommon {
             // -----------------------------------------------
             for (int i = curCount; i < countLetters; i++) {
                 String curLetter = alphaList.get(i).getText();
-                System.out.print(curLetter + " ");
+                logger.log(LogStatus.INFO, curLetter + " ");
                 // Get the result page for the letter clicked
                 ResultPage azListPage = dict.clickAZListLetter(i);
                 // Get the URL for the result page
@@ -295,7 +295,7 @@ public class NewTerms_Test extends NewDictionaryCommon {
                         AZLetterResultOK = true;
                     }
                     else {
-                        System.out.println(curLetter + " letter is empty!!!");
+                        logger.log(LogStatus.INFO, curLetter + " returns no results!!!");
                         AZLetterResultOK = false;
                     }
                 }
